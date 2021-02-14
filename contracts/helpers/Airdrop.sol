2ed629444fbf9566f6bc66e27707b42135390799
Araton95 <tonoyandeveloper@gmail.com>
2021-02-14 13:38:48 +0300

    Mainnet deployment and airdrop params changed

--- contracts/helpers/Airdrop.sol
+++ contracts/helpers/Airdrop.sol
@@ -13,18 +13,13 @@ import "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
 contract Airdrop {
   using SafeERC20 for IERC20;
 
-  IERC20 public token;
-
-  constructor (IERC20 _token) public {
-    token = _token;
-  }
-
   /**
    * @dev doAirdrop is the main method for distribution
+   * @param token airdropped token address (PIECE address)
    * @param addresses address[] addresses to airdrop
    * @param amounts address[] values for each address
    */
-  function doAirdrop(address[] calldata addresses, uint256 [] calldata amounts) external returns (uint256) {
+  function doAirdrop(IERC20 token, address[] calldata addresses, uint256 [] calldata amounts) external returns (uint256) {
     require(addresses.length > 0 && addresses.length == amounts.length, "doAirdrop: invalid input arrays!");
 
     uint256 i = 0;
@@ -40,7 +35,7 @@ contract Airdrop {
     receiver.transfer(address(this).balance);
   }
 
-  function emergencyExit(IERC20 _token, address receiver) external {
-    _token.safeTransfer(receiver, _token.balanceOf(address(this)));
+  function emergencyExit(IERC20 token, address receiver) external {
+    token.safeTransfer(receiver, token.balanceOf(address(this)));
   }
 }
