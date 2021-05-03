eb97025cc2ef70842bc1aa3fdd231d15066f9a44
Araton95 <tonoyandeveloper@gmail.com>
2021-05-03 12:13:46 +0300

    Deploy script modified

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,3 +1,4 @@
+const { network } = require("hardhat");
 const hre = require("hardhat");
 
 async function main() {
@@ -12,6 +13,7 @@ async function main() {
   const USDC_ADDRESS = "0xD87Ba7A50B2E7E660f678A895E4B72E7CB4CCd9C";
   const WETH_ADDRESS = "0xB4FBF271143F4FBf7B91A5ded31805e42b2208d6";
   const UNISWAP_FACTORY = "0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f";
+  const OPENSEA_PROXY = network === "rinkeby" ? "0xa5409ec958c83c3f309868babaca7c86dcb077c1" : "0xf57b2c51ded3a29e6891aba85459d600256cf317"
 
   // Roles
   const MINTER_ROLE = '0x9f2df0fed2c77648de5860a4cc508cd0818c85b8b8a1ab4ceeef8d981c8956a6';
@@ -21,18 +23,21 @@ async function main() {
   const MillionPieces = await hre.ethers.getContractFactory("MillionPieces");
   const PieceToken = await hre.ethers.getContractFactory("Piece");
   const Airdrop = await hre.ethers.getContractFactory("Airdrop");
-  const UniswapOracle = await hre.ethers.getContractFactory("UniswapOracle");
   console.log("Starting deployment ...");
 
   // Deploy MillionPieces NFT contract
-  const millionPieces = await MillionPieces.deploy(DEVELOPER_ADDRESS);
+  const millionPieces = await MillionPieces.deploy(DEVELOPER_ADDRESS, OPENSEA_PROXY);
   await millionPieces.deployed();
   console.log("MillionPieces NFT address:", millionPieces.address);
 
-  // // Deploy Auction contract
-  const auction = await Auction.deploy(millionPieces.address, FUND_ADDRESS);
+  // Deploy Auction contract
+  const auction = await Auction.deploy(millionPieces.address, FUND_ADDRESS, OPENSEA_PROXY);
   await auction.deployed();
   console.log("Auction address:", auction.address);
+
+  // Transfer auction ownership
+  await auction.transferOwnership(DEVELOPER_ADDRESS);
+  console.log("Ownership transferred");
 
   // Add auction as a minter
   await millionPieces.grantRole(MINTER_ROLE, auction.address);
